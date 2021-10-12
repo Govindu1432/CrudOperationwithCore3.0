bd9d10646ed7f8dc0b7f44caf4cc02f3485b0721
Govindu <govindu415417@gmail.com>
2021-10-12 12:33:01 +0530

    Add project files.

---
+++ CrudOperationwithCore3.0/Context/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using CoreDemo_3_0.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreDemo_3_0.Models;
+
+namespace CoreDemo_3_0.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() { }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<CoreDemo_3_0.Models.JobModel> JobModel { get; set; }
+    }
+
+
+}
